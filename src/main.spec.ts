288b076dc3e0ae4d07ebb5b39b89731f0030639d
xasopheno <danny@xasopheno.com>
2022-10-10 23:18:28 -0400

    WIP

---
+++ src/main.spec.ts
@@ -0,0 +1,107 @@
+import test from 'ava'
+import api from './api'
+import { commitForRelease, prepareEdit } from './main'
+import { Response } from 'node-fetch'
+
+test('commitForRelease()', (t) => {
+  t.is(
+    commitForRelease('This is a fixed commit message', {
+      formulaName: 'test formula',
+    }),
+    'This is a fixed commit message'
+  )
+  t.is(
+    commitForRelease('chore({{formulaName}}): version {{version}}', {
+      formulaName: 'test formula',
+    }),
+    'chore(test formula): version {{version}}'
+  )
+  t.is(
+    commitForRelease('{formulaName} {version}', {
+      formulaName: 'test formula',
+      version: 'v1.2.3',
+    }),
+    '{formulaName} {version}'
+  )
+  t.is(
+    commitForRelease('chore({{formulaName}}): upgrade to version {{version}}', {
+      formulaName: 'test formula',
+      version: 'v1.2.3',
+    }),
+    'chore(test formula): upgrade to version v1.2.3'
+  )
+  t.is(
+    commitForRelease(
+      '{{formulaName}} {{version}}: upgrade {{formulaName}} to version {{version}}',
+      {
+        formulaName: 'test formula',
+        version: 'v1.2.3',
+      }
+    ),
+    'test formula v1.2.3: upgrade test formula to version v1.2.3'
+  )
+  t.is(
+    commitForRelease('{{constructor}}{{__proto__}}', {}),
+    '{{constructor}}{{__proto__}}'
+  )
+  t.is(
+    commitForRelease('{{version}}', { version: 'v{{version}}' }),
+    'v{{version}}'
+  )
+})
+
+test('prepareEdit()', async (t) => {
+  const ctx = {
+    sha: 'TAGSHA',
+    ref: 'refs/tags/v0.8.2',
+    repo: {
+      owner: 'OWNER',
+      repo: 'REPO',
+    },
+  }
+
+  process.env['INPUT_HOMEBREW-TAP'] = 'Homebrew/homebrew-core'
+  process.env['INPUT_COMMIT-MESSAGE'] = 'Upgrade {{formulaName}} to {{version}}'
+
+  // FIXME: this tests results in a live HTTP request. Figure out how to stub the `stream()` method in
+  // calculate-download-checksum.
+  const stubbedFetch = function (url: string) {
+    if (url == 'https://api.github.com/repos/OWNER/REPO/tarball/v0.8.2') {
+      return Promise.resolve(
+        new Response('', {
+          status: 301,
+          headers: {
+            Location:
+              'https://github.com/mislav/bump-homebrew-formula-action/archive/v1.9.tar.gz',
+          },
+        })
+      )
+    }
+    throw url
+  }
+  const apiClient = api('ATOKEN', { fetch: stubbedFetch, logRequests: false })
+
+  const opts = await prepareEdit(ctx, apiClient, apiClient)
+  t.is(opts.owner, 'Homebrew')
+  t.is(opts.repo, 'homebrew-core')
+  t.is(opts.branch, '')
+  t.is(opts.filePath, 'Formula/repo.rb')
+  t.is(opts.commitMessage, 'Upgrade repo to 0.8.2')
+
+  const oldFormula = `
+    class MyProgram < Formula
+      revision 12
+      head "git://example.com/repo.git",
+        revision: "GITSHA"
+    end
+  `
+  t.is(
+    `
+    class MyProgram < Formula
+      head "git://example.com/repo.git",
+        revision: "GITSHA"
+    end
+  `,
+    opts.replace(oldFormula)
+  )
+})
